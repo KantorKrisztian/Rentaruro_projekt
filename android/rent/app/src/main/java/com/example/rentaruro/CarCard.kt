c5f5d7d0e78d244a8bd185a4f005c79b33831c9b
V Bende <varosi.bendeguz19k@egressy.info>
2025-05-12 13:22:33 +0200

    android

--- android/rent/app/src/main/java/com/example/rentaruro/CarCard.kt
+++ android/rent/app/src/main/java/com/example/rentaruro/CarCard.kt
@@ -4,9 +4,11 @@ package com.example.rentaruro
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.File
